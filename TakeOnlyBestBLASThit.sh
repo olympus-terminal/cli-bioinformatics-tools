5001a1f0ac9a09eadc347d4c96ed0a60658a7d35
Akai Tora <32707458+olympus-terminal@users.noreply.github.com>
2022-10-03 10:49:12 +0400

    Update TakeOnlyBestBLASThit.sh

--- TakeOnlyBestBLASThit.sh
+++ TakeOnlyBestBLASThit.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 ##where argv(1) are the blastp results in outputfmt 6 (tab delimited)##
 
 export LANG=C; export LC_ALL=C; sort -k1,1 -k12,12gr -k11,11g -k3,3gr "$1" | sort -u -k1,1 --merge > "$1"_TopHitsOnly.txt
